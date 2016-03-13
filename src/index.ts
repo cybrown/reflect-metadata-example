4ba8de1b2c554bd4bfc108997082cba00a472e1c
Cy Brown <cy.brown59@gmail.com>
2016-03-13 16:09:09 +0100

    Ajout decorator metadata

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,20 @@ function OnEvent(key: string): MethodDecorator {
     return (target: Object, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
         Reflect.defineMetadata('event', 'value', target);
         console.log(Reflect.getMetadata('event', target));
+        console.log(Reflect.getMetadata('design:type', target, propertyKey));
+        console.log(Reflect.getMetadata('design:paramtypes', target, propertyKey));
+        console.log(Reflect.getMetadata('design:returntype', target, propertyKey));
     }
+}
+
+class MyEventData {
+
 }
 
 class MyEmitter {
 
     @OnEvent('toto')
-    onToto() {
-
+    onToto(eventData: MyEventData): boolean {
+        return true;
     }
 }
